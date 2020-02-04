5f49ff4d4044d177020d81101a8d879b969267ca
Luiz Victor <luiz.victor100@hotmail.com>
2020-02-04 00:10:11 -0300

    iniciando projeto com express e ejs

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+//Escolhendo o ejs como (engine) reindenizador de html
+app.set("view engine","ejs");
+
+app.get("/:nome/:idade",(req, resp) => {
+    //render pega os arquivos *.ejs de dentro do diretório /views
+    var nome = req.params.nome;
+    var idade = req.params.idade;
+
+    var listas = [
+        {pessoa: "smith"},
+        {pessoa: "fulano"},
+        {pessoa: "beltrano"}
+    ];
+    resp.render("index",{
+        nome: nome,
+        idade: idade,
+        lista: listas
+    });
+});
+
+app.listen(8080,()=>{
+    console.log("Servidor rodando");
+});
